5d426e84954539085409bbd06073bcfe8e47872e
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-09-25 16:57:57 -0700

    mostrar una variable tipo fortnite 2 ;)

--- variables/mostrar_en_pantalla_variable_1.py
+++ variables/mostrar_en_pantalla_variable_1.py
@@ -1,2 +1,4 @@
-mensaje = "cargando_mensaje_espera_como_una_hora_por_que_fortinte_pesa_42G!!!"
-print(mensaje)
+mensaje_2 = "cargando_mensaje_espera_como_una_hora_por_que_fortinte_pesa_42G!!!"
+print(mensaje_2)
+mensaje_2 = "listoooo_ahora_pierde_tu_tiempo_con_estiloo!!!"
+print(mensaje_2)
