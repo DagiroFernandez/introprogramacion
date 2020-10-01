f9905921b35a5a5e621816079b624dd2cd8ec2e5
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-10-01 16:39:00 -0700

    ejercicio 3 de variables 2

---
+++ variables2/ejercicio3.py
@@ -0,0 +1,5 @@
+radio = float(input("introdusca el radio: "))
+area = 3.14 * radio**2
+perimetro = 2 * 3.14 * radio
+mensaje = f"Area del circulo es igual a {area} y el perimetro e sigual a{perimetro}"
+print(mensaje)
