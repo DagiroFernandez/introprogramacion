55a04b60ff95ba4d51f62ceb0a667310328a2f86
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-10-01 16:38:21 -0700

    ejercicio 1 de variables 2

---
+++ variables2/ejercicio2.py
@@ -0,0 +1,5 @@
+dividendo = float(input("introdusca dividendo: "))
+divisor = float(input("introdusca divisor: "))
+resto = dividendo % divisor
+mensaje = f"El resto obtenido al dividir {dividendo} entre {divisor} es {resto}"
+print(mensaje)
