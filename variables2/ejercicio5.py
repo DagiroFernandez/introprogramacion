56afaa026429b76fb5ef34d32a829ea67a6c26ab
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-10-01 17:57:11 -0700

    ejercicio 5 de variables 2

---
+++ variables2/ejercicio5.py
@@ -0,0 +1,13 @@
+num = int(input("ingrese el primmer nùmero: "))
+igual_a_5 = num == 5
+print(f"el numero es igual a 5: {igual_a_5}")
+float(num)
+igual_a_502 = num == 5.0
+print(f"el numero es igual a 5.0 : {igual_a_502}")
+int(num)
+mayor_0_menor10 = num > 0 and num <10
+print(f"el numero es mayor que 0 y menor que 10: {mayor_0_menor10}")
+menor_0_mayor10 = num < 0 and num > 10
+print(f"el numero es menor que 0 y mayor que 10: {menor_0_mayor10} ")
+menor_final = num == 5 and num > 10 and num < 20
+print(f"El número es igual a 5, o en caso de no serlo, si es mayor que 10 y menor que 20: {menor_final} ")
