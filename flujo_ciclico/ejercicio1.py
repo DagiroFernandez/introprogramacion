ea8b689769ab688d96777087d43b0a138f191512
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-10-06 16:22:52 -0700

    numero negativo

---
+++ flujo_ciclico/ejercicio1.py
@@ -0,0 +1,4 @@
+num = int(input("Ingresa un numero positivo por favor: "))
+while num < 0:
+    print("El numero ingresado no es positivo, ingrese otro por favor")
+    num = int(input("Ingrese otro numero: "))
