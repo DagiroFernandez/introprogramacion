b67d095093f28119ea46dbfc0742f2c600861a07
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-10-09 17:09:04 -0700

    numero ordenado 1.0

---
+++ practico_1/ejercicio6.py
@@ -0,0 +1,12 @@
+numero1=int(input("ingrse un numero: "))
+numero2 =int(input("ingrse otro numero: "))
+if numero1 > numero2:
+    print(numero1 , numero2)
+elif numero2 < numero1:
+    print(numero2, numero1)
+elif numero1 == numero2:
+    print("Los dos numero son iguales")
+
+
+
+
