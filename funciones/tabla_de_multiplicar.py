a10585691dc797aeb40b4884d5418da08ed3c78f
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-10-22 16:19:15 -0700

    tabla de multiplicar

---
+++ funciones/tabla_de_multiplicar.py
@@ -0,0 +1,20 @@
+def tablaM(num):
+    for i in range(1,11):
+        print(num, "x" , "=", i * num)
+    print("tabla de multiplicar de", num)
+
+
+
+
+num=tablaM(1)
+num=tablaM(2)
+num=tablaM(3)
+num=tablaM(4)
+num=tablaM(5)
+num=tablaM(6)
+num=tablaM(7)
+num=tablaM(8)
+num=tablaM(9)
+num=tablaM(10)
+
+
