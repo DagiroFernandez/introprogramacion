2be7becb3ac813c4d4a22697b7395551463a93a4
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-10-27 16:08:29 -0700

    longitud de una cadena

--- funciones/longitud_de_una_cxadena.py
+++ funciones/longitud_de_una_cxadena.py
@@ -3,8 +3,5 @@ def longcadena(x):
     for i in (x):
         contador= contador +1
     return(contador)
-def cadena(x):
-    j= x.lower()
-    return j[0].upper()+j[1:]
 x =input("Ingrese una cadena: ")
-print(cadena(x), "tiene", longcadena(x), "caracteres")
+print(x, "tiene", longcadena(x), "caracteres")
