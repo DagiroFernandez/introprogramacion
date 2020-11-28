da3582c019e30312376379679d9a02fba6817a8f
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-11-27 16:51:15 -0800

    array

--- interfaz_grafica_python/array.py
+++ interfaz_grafica_python/array.py
@@ -2,13 +2,14 @@ import random
 numeros_correcto=[]
 array=[]
 tamaño=int(input("ingrese el tamaño de su array: "))
+digito_deseado=input("ingrese el digito buscado: ")
 for i in range(0,tamaño):
     num= random.randint(0,300)
     array.append(num)
     num=str(num)
     for i in range(0,tamaño):
         ultimo_digito= num[len(num)-1]
-    if ultimo_digito=="5":
+    if ultimo_digito==digito_deseado:
         numeros_correcto.append(num)
 
 print(numeros_correcto)
