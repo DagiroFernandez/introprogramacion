17b5d07bcdc73d969cc0d2ec4c5da86295957b91
DagiroFernandez <dagiro.fernandez@ucb.edu.bo>
2020-12-01 16:39:48 -0800

    ejercicio_2

---
+++ interfaz_grafica_python/ejercicio_2.py
@@ -0,0 +1,17 @@
+numi=int(input("ingresa el numero inicial: "))
+numf=int(input("ingrese en numero final: "))
+numdenrango=[]
+suma=0
+suma2=0
+for i in range(numf,numi-1,-1):
+    print( i,end="")
+print("")
+
+for i in range(numf,numi-1,-1):
+    if i%2==0:
+        suma=suma+i
+    else:
+        suma2=suma2 +i
+print("la suma de los pares es",suma,end="")
+print("")
+print("la suma de los impares es",suma2)
